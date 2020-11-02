6a26ce8dc235da4e53ee345a881db325cc4469b7
fs2050 <fabiolfs76@hotmail.com>
2020-11-01 23:03:43 -0300

    Operadores Matematicos

---
+++ Operadores/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores{
+	public  static void main(String args[]){
+	
+	  int num1 = 10;
+	  int num2 = 2;
+	  //int resultado = num1 - num2;
+	  
+	  
+	  System.out.println(num1 +  num2);
+	   System.out.println(num1 - num2);
+	    System.out.println(num1 / num2);
+		 System.out.println(num1 * num2);
+		  System.out.println(num1 %  num2);
+	  
+	  
+	
+	}
+	
+	
+	
+	
+}
