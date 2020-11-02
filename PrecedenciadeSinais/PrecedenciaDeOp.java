2061cb9b64e25a7c75006d2e0c2c8db5fef6d971
fs2050 <fabiolfs76@hotmail.com>
2020-11-01 23:30:01 -0300

    Precedencia de Operadores

---
+++ PrecedenciadeSinais/PrecedenciaDeOp.java
@@ -0,0 +1,20 @@
+//Precedencia de operadores - prioridade de sinais
+
+public class PrecedenciaDeOp{
+	public  static void main(String args[]){
+	
+	  int num1 = 10;
+	  int num2 = 2;
+	  int resultado = (num1 +  5) * num2 / (10 - 3);
+	  
+	  
+	  System.out.println(resultado);
+	  
+	  
+	
+	}
+	
+	
+	
+	
+}
