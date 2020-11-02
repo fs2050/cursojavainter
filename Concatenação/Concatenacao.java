987abda5f8e99cc964432909b14901ffa5f30745
fs2050 <fabiolfs76@hotmail.com>
2020-11-01 23:43:45 -0300

    Concatenação

---
+++ Concatenação/Concatenacao.java
@@ -0,0 +1,25 @@
+//Precedencia de operadores - prioridade de sinais
+
+public class Concatenacao{
+	public  static void main(String args[]){
+	
+	  int num1 = 42;
+	  int num2 = 2;
+	
+	  String nome = "Fabio ";
+	  String snome = "Salles ";
+	  
+	    int resultado = num1 +  num2 ;
+		String nomeCompleto = nome + snome + "tem " + resultado + " anos de idade.";
+	  
+	  System.out.println(resultado);
+	  System.out.println(nomeCompleto);
+	  
+	  
+	
+	}
+	
+	
+	
+	
+}
